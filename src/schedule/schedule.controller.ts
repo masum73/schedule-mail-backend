482fb2c45a8a30d0782d0e075ab5ebd328456c0a
Wahid Sakib <wsakib87@gmail.com>
2023-09-07 17:00:28 +0600

    created wahid branch

---
+++ src/schedule/schedule.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { ScheduleService } from './schedule.service';
+
+@Controller('schedule')
+export class ScheduleController {
+  constructor(private readonly scheduleService: ScheduleService) {}
+  @Get()
+  public getSchedules(): Promise<any> {
+    return this.scheduleService.findAll();
+  }
+}
---
+++ src/schedule/schedule.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ScheduleService {
+  constructor() {}
+
+  public async findAll() {
+    return [];
+  }
+}
